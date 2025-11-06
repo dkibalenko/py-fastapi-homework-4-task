61b843b62d3e5dad048ffed9cab7b5ed803b4741
Denys Kibalenko <dennieking87@gmail.com>
2025-11-06 18:32:50 +0100

    Refactor: remove `finally` block in `validate_image` validator in `src/validation/profile.py`

--- src/validation/profile.py
+++ src/validation/profile.py
@@ -33,9 +33,6 @@ def validate_image(avatar: UploadFile) -> UploadFile:
             )
     except IOError:
         raise ValueError("Invalid image format")
-    finally:
-        if hasattr(image, "fp") and image.fp:
-            image.close()
 
     return avatar
 
